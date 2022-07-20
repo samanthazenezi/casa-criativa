366c60d77f326a44a22c66022900427e47165e64
Samantha Zenezi <samanthazenezi@gmail.com>
2022-07-19 21:19:05 -0300

    casa criativa

--- db.js
+++ db.js
@@ -14,30 +14,42 @@ db.serialize(function() {
         );
     `)
     //INSERIR DADOS NA TABELA
-    const query = `
-    INSERT INTO ideias(
-        image,
-        title,
-        category,
-        description,
-        link
-    ) VALUES (?,?,?,?,?);
-    `
-    const values = [
-        "assets/curso-dev.png",
-        "Cursos de programação",
-        "Estudo",
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti impedit aperiam molestiae corporis dolorum dolores aut obcaecati excepturi aliquid quam ratione incidunt quia dolore ipsam ut doloremque, repellendus error provident?",
-        "https://www.rocketseat.com.br/"
-    ]
-    db.run(query, values, function(err) {
-        if (err) return console.log(err)
+    // const query = `
+    //     INSERT INTO ideias(
+    //         image,
+    //         title,
+    //         category,
+    //         description,
+    //         link
+    //     )   VALUES (?,?,?,?,?);
+    // `
+    // const values = [
+    //      "assets/curso-dev.png",
+    //      "Cursos de programação",
+    //      "Estudo",
+    //      "Estudar programação para fazer aplicações incríveis!!",
+    //      "https://www.rocketseat.com.br/"
+    //     ]
+    //     db.run(query, values, function(err){
+    //     if (err) return console.log(err)
 
-        console.log(this)
-    })
-
-    //CONSULTAR DADOS NA TABELA
+    //     console.log(this)
+    // })
 
     //DELETAR UM DADO DA TABELA
+    //db.run (`DELETE FROM ideias WHERE id = ?`, [1], function(err){
+        //if (err) return console.log(err)
 
+        //console.log("DELETEI", this)
+    //})
+
+    //CONSULTAR DADOS NA TABELA
+    //db.all(`SELECT * FROM ideias`, function(err, rows) {
+        //if (err) return console.log(err)
+
+        //console.log(rows)
+    //})
 })
+
+//USANDO O BANCO DE DADOS NA MINHA APLICAÇÃO
+module.exports = db
--- server.js
+++ server.js
@@ -2,41 +2,14 @@
 const express = require("express")
 const server = express()
 
-//criando variáveis para o HTML com nunjucks
-const ideias = [
-    {
-        img: "assets/curso-dev.png",
-        title: "Cursos de programação",
-        category: "Estudo",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti impedit aperiam molestiae corporis dolorum dolores aut obcaecati excepturi aliquid quam ratione incidunt quia dolore ipsam ut doloremque, repellendus error provident?",
-        url: "https://www.rocketseat.com.br/"
-    },
-    {
-        img: "assets/exercicios.png",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti impedit aperiam molestiae corporis dolorum dolores aut obcaecati excepturi aliquid quam ratione incidunt quia dolore ipsam ut doloremque, repellendus error provident?",
-        url: "https://www.youtube.com/c/CaioSignoretti"
-    },
-    {
-        img: "assets/yoga.png",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti impedit aperiam molestiae corporis dolorum dolores aut obcaecati excepturi aliquid quam ratione incidunt quia dolore ipsam ut doloremque, repellendus error provident?",
-        url: "https://www.youtube.com/c/PriLeiteYoga"
-    },
-    {
-        img: "assets/jardinagem.png",
-        title: "Jardinagem",
-        category: "Meio ambiente",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti impedit aperiam molestiae corporis dolorum dolores aut obcaecati excepturi aliquid quam ratione incidunt quia dolore ipsam ut doloremque, repellendus error provident?",
-        url: "https://www.youtube.com/watch?v=t_UDYuByuYo&list=PLQMyk6-jAVtpnOh8370lhH2cvXT6W9Vqh"
-    },
-
-]
+//utilizando meu banco de dados
+const db = require("./db")
 
 //configurar arquivos estáticos (css, scripts e imagens)
 server.use(express.static("public"))
+
+//habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }))
 
 //configuração do nunjucks
 const nunjucks = require("nunjucks")
@@ -49,23 +22,59 @@ nunjucks.configure("views", {
 // e capturo o pedido do clinte para responder
 server.get("/", function(req, res) {
 
-    const reversedIdeias = [...ideias].reverse()
+    db.all(`SELECT * FROM ideias`, function(err, rows) {
+        if (err) {
+            console.log("Erro no banco de dados!")
+        }
 
-    let lastIdeias = []
-    for (let ideia of reversedIdeias) {
-        if(lastIdeias.length < 2) {
-            lastIdeias.push(ideia)
-        } 
-    }
-    return res.render("index.html", { ideias: lastIdeias })
+        const reversedIdeias = [...rows].reverse()
+
+        let lastIdeias = []
+        for (let ideia of reversedIdeias) {
+            if(lastIdeias.length < 2) {
+                lastIdeias.push(ideia)
+            } 
+        }
+        return res.render("index.html", { ideias: lastIdeias })
+    })
 })
 server.get("/ideias", function(req, res) {
+    db.all(`SELECT * FROM ideias`, function(err, rows) {
+        if (err) {
+            console.log("Erro no banco de dados!")
+        }
+        const reversedIdeias = [...rows].reverse()
 
-    const reversedIdeias = [...ideias].reverse()
-
-    return res.render("ideias.html", {ideias: reversedIdeias})
+        return res.render("ideias.html", {ideias: reversedIdeias})
+    }) 
 })
+server.post("/", function(req, res){
+    //INSERIR DADOS NA TABELA
+    const query = `
+        INSERT INTO ideias(
+            image,
+            title,
+            category,
+            description,
+            link
+        )   VALUES (?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link,
+    ]
 
+    db.run(query, values, function(err){
+        if (err) {
+            console.log("Erro no banco de dados!")
+        }
+
+        return res.redirect("/ideias")
+    })
+})
 
 //liguei meu servidor na porta 3000
 server.listen(3000)
